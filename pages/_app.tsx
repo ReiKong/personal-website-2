75b0b9fef01e6af6748cb47199a940cdf50d01a0
Rei Kong <42074120+ReiKong@users.noreply.github.com>
2023-10-18 10:54:18 -0400

    Delete pages directory

--- pages/_app.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d4ac5c6a98a4fbe5e0d16536c5254aa508a17721753fec8572dc4ee570a39585
-size 662
--- pages/index.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aa8a6a2c10e9245ab67f2b5e703b4eed7070106aeaf14d16cce7ab28119af294
-size 115

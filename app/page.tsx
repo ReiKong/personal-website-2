e0cc8747c577e736f2077bff213cd7c98622ec90
Rei Kong <reiikongs@gmail.com>
2023-12-21 14:40:43 -0600

    Added sticky scroll, fixing typefaces not appearing on other devices

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
 import SideBar from '../components/sideBar'
 import MainContent from '../components/mainContent'
+import Footer from '../components/navigation/footer'
 
 export default function Home() {
   return (
-      <div className="sweIndex" data-theme="beige">
-        <SideBar/>
-        <MainContent/>
+    <div className="flex flex-col">
+      <div className="sweIndex">
+        <SideBar />
+        <MainContent />
       </div>
+      <Footer />
+    </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,20 +5,26 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './data/*.{js}'
   ],
   theme: {
-    colors: {
-      primary: 'rgb(var(--color-primary) / <alpha-value>)',
-      secondary1: 'rgb(var(--color-text1) / <alpha-value>)',
-      secondary2: 'rgb(var(--color-text2) / <alpha-value>)',
-      secondary3: 'rgb(var(--color-text3) / <alpha-value>)',
-      accent: 'rgb(var(--color-accent) / <alpha-value>)',
-    },
-    fontFamily: {
-      'sans': ['"Acumin Pro"', 'ui-sans-serif', 'system-ui'],
-      'serif': ['"Meno Banner Condensed"', 'ui-serif'],
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary1: 'rgb(var(--color-text1) / <alpha-value>)',
+        secondary2: 'rgb(var(--color-text2) / <alpha-value>)',
+        secondary3: 'rgb(var(--color-text3) / <alpha-value>)',
+        accent: 'rgb(var(--color-accent) / <alpha-value>)',
+      },
+      fontFamily: {
+        'sans': ['"Acumin Pro"'],
+        'serif': ['"Meno Banner Condensed"'],
+        'body': ['"Acumin Pro"'],
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
 export default config

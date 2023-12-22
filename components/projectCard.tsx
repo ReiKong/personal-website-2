c64c884801113dd857e455ae7f8d91327779fc8f
Rei Kong <reiikongs@gmail.com>
2023-12-22 05:20:03 -0600

    Fixed project card borders

--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -14,7 +14,7 @@ export default function projectCard({
     techStack: string;
 }) {
     return (
-        <div className="flex-col justify-start items-start gap-2.5 inline-flex">
+        <div className="flex-col justify-start items-start gap-2.5 inline-flex border-secondary1 border-[1px] border-collapse mt-[-1px] mr-[-1px]">
             <div></div>
             <div className="px-2.5 pb-2.5 flex-col justify-start items-start gap-2.5 inline-flex">
                 <p className="font-medium text-[20px]">{title}</p>

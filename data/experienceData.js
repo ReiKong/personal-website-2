b3113def081a482d180bdc6d9c9d3a6f8a30ec2f
Rei Kong <42074120+ReiKong@users.noreply.github.com>
2025-07-10 10:07:41 +0800

    Update experienceData.js

--- data/experienceData.js
+++ data/experienceData.js
@@ -1,8 +1,48 @@
 export const experience = [
     {
+        time: "August 2025 – December 2025",
+        role: "Undergraduate Teaching Assistant",
+        company: "McGill University",
+        responsibilities: [
+            ["Host office hours, answer technical questions, and create comprehension quizzes and other educational content on Python and data structures for 900+ students"],
+        ],
+        techStack: [["Python"]],
+    },
+    {
+        time: "June 2025 – August 2025",
+        role: "Software Engineer Intern",
+        company: "Amazon",
+        responsibilities: [
+            ["Engineered a scalable command-line interface in Java to handle mass AWS service migrations, reducing pipeline migration time from 12 weeks to 4 weeks for 10,000+ engineers"],
+            ["Authored and presented a system design document analyzing performance trade-offs and scalability to 3 stakeholder groups"],
+            ["Diagnosed a production issue with backend access and authentication paths and led improvements to authentication logic, impacting 2,000+ teams"],
+            ["Wrote internal documentation adopted as a team standard, detailing code purpose, scalability, and future design gaps"],
+            ["Proactively created a system design diagram of the team’s full-stack web application to support onboarding and cross-team collaboration"],
+        ],
+        techStack: [["Java"]],
+    },
+    {
+        time: "October 2024 – December 2024",
+        role: "Research Intern",
+        company: "McGill University",
+        responsibilities: [
+            ["Authored a 10-page literature review analyzing bias in AI-driven resume screening tools, synthesizing existing research to identify underlying causes of bias and propose technical and non-technical mitigation strategies"],
+        ],
+    },
+    {
+        time: "October 2023 – October 2024",
+        role: "Software Engineer Intern",
+        company: "Business-Software.com",
+        responsibilities: [
+            ["Created a JavaScript-based Chrome extension, increasing filled-in forms by 100%"],
+            ["Leveraged Chrome Extension APIs to automate repetitive tasks, reducing time to input form data by 80%"],
+        ],
+        techStack: [["JavaScript"]],
+    },
+    {
         time: "May 2023 – August 2023",
-        role: "Frontend Developer & Designer",
-        company: "Biotechnology Research Startup",
+        role: "Web Developer Intern & Designer",
+        company: "Cellterix",
         responsibilities: [
             ["Developed a website for a biotechnology startup using React.js, Tailwind CSS, and HTML"],
             ["Conceptualized and designed the website, brand identity, and other creative collaterals with Figma and Adobe Illustrator"],
@@ -10,3 +50,9 @@ export const experience = [
         techStack: [["React.js"], ["Tailwind CSS"], ["HTML"],],
     },
 ];
+
+
+Created a JavaScript-based Chrome extension, increasing filled-in forms by 100%
+
+
+

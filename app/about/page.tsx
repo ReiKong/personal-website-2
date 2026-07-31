d681174b74999632f639576d83db248b13c9192f
Rei Kong <reiikongs@gmail.com>
2026-07-31 15:28:31 -0500

    test

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,7 +12,12 @@ export default function AboutPage() {
             <div className="sweIndex">
                 <SideBar />
                 <main className="top-0 bottom-0 right-0 pt-[40px] px-10 pb-10 relative min-h-[100vh] border-b-[1px] border-b-secondary1">
-                    <WorkInProgressHero id="about-wip" />
+                    {/* <WorkInProgressHero id="about-wip" /> */}
+                    <p>
+                        I care about craft, grit, and raising the bar—for myself, my work, and the people around me—while always making room for good banter and better questions.
+
+                        Previously built software at Amazon to facilitate large-scale service migrations between AWS regions for thousands of teams.
+                    </p>
                 </main>
             </div>
             <Footer />
